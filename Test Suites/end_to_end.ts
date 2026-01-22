61f3a063a78d411f825a7424b9a7916fd5f5166e
akbarlaptop <akbridha@gmail.com>
2026-01-22 21:00:50 +0800

    Buat Test Suite

---
+++ Test Suites/end_to_end.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>end_to_end</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dc959f1e-d652-4f70-b50f-6a257134b884</testSuiteGuid>
+</TestSuiteEntity>
